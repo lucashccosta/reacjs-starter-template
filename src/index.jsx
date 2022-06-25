cd5de5d27f66f1f03cd41499ec41c9377485870d
Lucas Costa <lucas.hcruz.costa@gmail.com>
2022-06-25 11:05:19 -0300

    fix: alterado forma de renderizar o app

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
-import { render } from 'react-dom';
-import { App } from './App'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import { App } from './App';
 
-render(
-    <App />, 
-    document.getElementById('root')
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
